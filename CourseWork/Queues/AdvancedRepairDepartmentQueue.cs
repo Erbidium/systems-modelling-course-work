5e9a0295c98e3e4ba152bab16487a36550a0b0c5
Erbidium <OlexandrTanasienko@gmail.com>
2023-12-23 17:14:51 +0200

    Implement advanced repair queue

---
+++ CourseWork/Queues/AdvancedRepairDepartmentQueue.cs
@@ -0,0 +1,21 @@
+﻿using Lab3.Items;
+
+namespace Lab3.Queues;
+
+public class AdvancedRepairDepartmentQueue : RepairDepartmentQueue
+{
+    public override SimpleItem Remove()
+    {
+        var nodes = Items.Select(i => (i as Node)!).ToList();
+        var returnedItems = nodes.Where(n => n.ReturnsCount > 0).ToList();
+
+        if (returnedItems.Count == 0)
+        {
+            return base.Remove();
+        }
+        
+        var itemToRemove = returnedItems.MaxBy(n => n.ReturnsCount * n.RepairTime)!;
+        Items.Remove(itemToRemove);
+        return itemToRemove;
+    }
+}
